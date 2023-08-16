f974bb979f21f387f0da1c94bd2caddc8b1c1a45
Stanislav Latsko <01latsko10@gmail.com>
2023-08-16 11:36:51 +0200

    now data is read from url

--- src/main/java/test_quiz/entities/services/QuestionService.java
+++ src/main/java/test_quiz/entities/services/QuestionService.java
@@ -1,6 +1,7 @@
 package test_quiz.entities.services;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.json.JSONTokener;
 import test_quiz.entities.Answer;
 import test_quiz.entities.Question;
@@ -18,22 +19,12 @@ public class QuestionService {
         URL url = new URL("https://api.jsonbin.io/v3/b/64db27789d312622a3916a70");
         HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
 
-        String line = "";
-        if(httpsURLConnection.getResponseCode() == 200) {
-            InputStream inputStream = httpsURLConnection.getInputStream();
-
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            line = bufferedReader.readLine();
-        }
-        String test = line.substring(10, line.indexOf("metadata")-2);
-        System.out.println(line);
-        System.out.println(test);
-
-        JSONArray read = new JSONArray(new JSONTokener(new FileInputStream("questions.json")));
-        for (int i = 0; i < read.length(); i++) {
-            String questionContent = read.getJSONObject(i).getString("question");
-            String answer = read.getJSONObject(i).getString("answer");
-            JSONArray options = read.getJSONObject(i).getJSONArray("options");
+        JSONObject readObject = new JSONObject(new JSONTokener(httpsURLConnection.getInputStream()));
+        JSONArray objects = readObject.getJSONArray("record");
+        for (int i = 0; i < objects.length(); i++) {
+            String questionContent = objects.getJSONObject(i).getString("question");
+            String answer = objects.getJSONObject(i).getString("answer");
+            JSONArray options = objects.getJSONObject(i).getJSONArray("options");
             List<String> stringOptions = new ArrayList<>();
             for (int j = 0; j < 4; j++) {
                 stringOptions.add(options.getString(j));
